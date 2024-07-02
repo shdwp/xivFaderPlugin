2355ea37c8f21ccdf84d70d41975aada679e9a07
Infi <infiziert@protonmail.ch>
2024-07-02 07:15:30 +0200

    Add Viper and Pictomancer, small cleanup

--- FaderPlugin/Config/Config.cs
+++ FaderPlugin/Config/Config.cs
@@ -2,59 +2,56 @@
 using System.Collections.Generic;
 using Dalamud.Configuration;
 
-namespace FaderPlugin.Config {
+namespace FaderPlugin.Config;
 
-    public class ConfigEntry {
-        public State state { get; set; }
-        public Setting setting { get; set; }
+public class ConfigEntry {
+    public State state { get; set; }
+    public Setting setting { get; set; }
 
-        public ConfigEntry(State state, Setting setting) {
-            this.state = state;
-            this.setting = setting;
+    public ConfigEntry(State state, Setting setting) {
+        this.state = state;
+        this.setting = setting;
+    }
+}
+
+[Serializable]
+public class Config : IPluginConfiguration {
+    public event Action? OnSave;
+
+    public int Version { get; set; } = 6;
+    public Dictionary<Element, List<ConfigEntry>> elementsConfig { get; set; }
+    public bool DefaultDelayEnabled { get; set; } = true;
+    public int DefaultDelay { get; set; } = 2000;
+    public int ChatActivityTimeout { get; set; } = 5 * 1000;
+    public int OverrideKey { get; set; } = 0x12;
+    public bool FocusOnHotbarsUnlock { get; set; } = false;
+    public bool EmoteActivity { get; set; } = false;
+    public bool ImportantActivity { get; set; } = false;
+
+    public void Initialize() {
+        // Initialise the config.
+        elementsConfig ??= new Dictionary<Element, List<ConfigEntry>>();
+
+        foreach(Element element in Enum.GetValues(typeof(Element))) {
+            if(!elementsConfig.ContainsKey(element)) {
+                List<ConfigEntry> entry = new() { new ConfigEntry(State.Default, Setting.Show) };
+                elementsConfig[element] = entry;
+            }
         }
+
+        Save();
     }
 
-    [Serializable]
-    public class Config : IPluginConfiguration {
-        public event Action? OnSave;
-
-        public int Version { get; set; } = 6;
-        public Dictionary<Element, List<ConfigEntry>> elementsConfig { get; set; }
-        public bool DefaultDelayEnabled { get; set; } = true;
-        public int DefaultDelay { get; set; } = 2000;
-        public int ChatActivityTimeout { get; set; } = 5 * 1000;
-        public int OverrideKey { get; set; } = 0x12;
-        public bool FocusOnHotbarsUnlock { get; set; } = false;
-        public bool EmoteActivity { get; set; } = false;
-        public bool ImportantActivity { get; set; } = false;
-
-        public void Initialize() {
-            // Initialise the config.
-            if (elementsConfig == null) {
-                elementsConfig = new Dictionary<Element, List<ConfigEntry>>();
-            }
-
-            foreach(Element element in Enum.GetValues(typeof(Element))) {
-                if(!elementsConfig.ContainsKey(element)) {
-                    List<ConfigEntry> entry = new() { new ConfigEntry(State.Default, Setting.Show) };
-                    elementsConfig[element] = entry;
-                }
-            }
-
-            Save();
+    public List<ConfigEntry> GetElementConfig(Element elementId) {
+        if(!elementsConfig.ContainsKey(elementId)) {
+            elementsConfig[elementId] = [];
         }
 
-        public List<ConfigEntry> GetElementConfig(Element elementId) {
-            if(!elementsConfig.ContainsKey(elementId)) {
-                elementsConfig[elementId] = new();
-            }
+        return elementsConfig[elementId];
+    }
 
-            return elementsConfig[elementId];
-        }
-
-        public void Save() {
-            Plugin.PluginInterface.SavePluginConfig(this);
-            OnSave?.Invoke();
-        }
+    public void Save() {
+        Plugin.PluginInterface.SavePluginConfig(this);
+        OnSave?.Invoke();
     }
 }
--- FaderPlugin/Config/State.cs
+++ FaderPlugin/Config/State.cs
@@ -1,72 +1,71 @@
 ﻿using System.Collections.Generic;
 
-namespace FaderPlugin.Config {
-    // Do not change the values of existing states, it will break configs
-    public enum State {
-        None = 0,
-        Default = 1,
-        Duty = 2,
-        EnemyTarget = 3,
-        PlayerTarget = 4,
-        NPCTarget = 5,
-        Crafting = 6,
-        Gathering = 7,
-        Mounted = 8,
-        Combat = 9,
-        WeaponUnsheathed = 10,
-        IslandSanctuary = 11,
-        ChatFocus = 12,
-        UserFocus = 13,
-        ChatActivity = 14,
-        AltKeyFocus = 15,
-        CtrlKeyFocus = 16,
-        ShiftKeyFocus = 17,
-        InSanctuary = 18,
-    }
+namespace FaderPlugin.Config;
 
-    public static class StateUtil {
-        public static string GetStateName(State state)
+// Do not change the values of existing states, it will break configs
+public enum State {
+    None = 0,
+    Default = 1,
+    Duty = 2,
+    EnemyTarget = 3,
+    PlayerTarget = 4,
+    NPCTarget = 5,
+    Crafting = 6,
+    Gathering = 7,
+    Mounted = 8,
+    Combat = 9,
+    WeaponUnsheathed = 10,
+    IslandSanctuary = 11,
+    ChatFocus = 12,
+    UserFocus = 13,
+    ChatActivity = 14,
+    AltKeyFocus = 15,
+    CtrlKeyFocus = 16,
+    ShiftKeyFocus = 17,
+    InSanctuary = 18,
+}
+
+public static class StateUtil {
+    public static string GetStateName(State state)
+    {
+        return state switch
         {
-            return state switch
-            {
-                State.EnemyTarget => "Enemy Target",
-                State.PlayerTarget => "Player Target",
-                State.NPCTarget => "NPC Target",
-                State.WeaponUnsheathed => "Weapon Unsheathed",
-                State.InSanctuary => "In Sanctuary",
-                State.IslandSanctuary => "Island Sanctuary",
-                State.ChatActivity => "Chat Activity",
-                State.ChatFocus => "Chat Focus",
-                State.UserFocus => "User Focus",
-                State.AltKeyFocus => "ALT Key Focus",
-                State.CtrlKeyFocus => "CTRL Key Focus",
-                State.ShiftKeyFocus => "SHIFT Key Focus",
-                _ => state.ToString()
-            };
-        }
-
-        public static readonly List<State> orderedStates = new() {
-            State.None,
-            State.Default,
-            State.Duty,
-            State.EnemyTarget,
-            State.PlayerTarget,
-            State.NPCTarget,
-            State.Crafting,
-            State.Gathering,
-            State.Mounted,
-            State.Combat,
-            State.WeaponUnsheathed,
-            State.InSanctuary,
-            State.IslandSanctuary,
-            State.ChatActivity,
-            State.ChatFocus,
-            State.UserFocus,
-            State.AltKeyFocus,
-            State.CtrlKeyFocus,
-            State.ShiftKeyFocus,
+            State.EnemyTarget => "Enemy Target",
+            State.PlayerTarget => "Player Target",
+            State.NPCTarget => "NPC Target",
+            State.WeaponUnsheathed => "Weapon Unsheathed",
+            State.InSanctuary => "In Sanctuary",
+            State.IslandSanctuary => "Island Sanctuary",
+            State.ChatActivity => "Chat Activity",
+            State.ChatFocus => "Chat Focus",
+            State.UserFocus => "User Focus",
+            State.AltKeyFocus => "ALT Key Focus",
+            State.CtrlKeyFocus => "CTRL Key Focus",
+            State.ShiftKeyFocus => "SHIFT Key Focus",
+            _ => state.ToString()
         };
     }
 
-
+    public static readonly List<State> orderedStates =
+    [
+        State.None,
+        State.Default,
+        State.Duty,
+        State.EnemyTarget,
+        State.PlayerTarget,
+        State.NPCTarget,
+        State.Crafting,
+        State.Gathering,
+        State.Mounted,
+        State.Combat,
+        State.WeaponUnsheathed,
+        State.InSanctuary,
+        State.IslandSanctuary,
+        State.ChatActivity,
+        State.ChatFocus,
+        State.UserFocus,
+        State.AltKeyFocus,
+        State.CtrlKeyFocus,
+        State.ShiftKeyFocus
+    ];
 }
